2f49f5d8b8e3b8f0f780973e38601bfb01d80ac4
Robert Dubrovski <robertdubrovski@gmail.com>
2015-07-20 15:04:53 -0400

    print empty grid before solving

--- sudoku.rb
+++ sudoku.rb
@@ -13,6 +13,9 @@ class Sudoku
   end # end of initialize
 
   def setup(grid)
+
+  puts "This is your grid:"
+  Sudoku.print_grid(grid)
   
     grid.each do |y|
 
@@ -218,7 +221,7 @@ line = 1
 gameboard = []
 while line < 10 do
 
-puts "Enter line ##{line} of your Sudoku below. For example, \"1 3 5 7 9\". If you make a mistake, type \"undo\" to redo the previous line."
+puts "Enter line ##{line} of your Sudoku below. For example, \"1 3 5 7 9\". If you make a mistake, type \"undo\" to re-enter the previous line."
 input = gets.chomp
 
 if input == "undo"
